f35f0b47447f65354e40227e44eda59e5ac4a1bc
Simon Schmid <mail@sschmid.com>
2021-03-13 17:44:08 +0100

    Add github 1.0.0

---
+++ github/1.0.0/plugin.sh
@@ -0,0 +1,11 @@
+BEE_PLUGIN_NAME="github"
+BEE_PLUGIN_VERSION="1.0.0"
+BEE_PLUGIN_LICENSE="MIT"
+BEE_PLUGIN_HOMEPAGE="https://github.com/sschmid/bee-github"
+BEE_PLUGIN_AUTHORS="sschmid"
+BEE_PLUGIN_INFO="Useful commands for GitHub"
+BEE_PLUGIN_SOURCE_HTTPS="https://github.com/sschmid/bee-github.git"
+BEE_PLUGIN_SOURCE_SSH="git@github.com:sschmid/bee-github.git"
+BEE_PLUGIN_TAG="1.0.0"
+BEE_PLUGIN_SHA256="63dcd415871218ede4c01995137731915f9532ef55c44843a2d47257d71c9e75"
+BEE_PLUGIN_DEPENDENCIES=(version)
