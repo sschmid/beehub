09505af4efaa98852a572eb02ddd1fa0180e63e1
Simon Schmid <mail@sschmid.com>
2021-03-13 17:37:10 +0100

    Add sample 1.0.0

---
+++ sample/1.0.0/plugin.sh
@@ -0,0 +1,12 @@
+BEE_PLUGIN_NAME="sample"
+BEE_PLUGIN_VERSION="1.0.0"
+BEE_PLUGIN_LICENSE="MIT"
+BEE_PLUGIN_HOMEPAGE="https://github.com/sschmid/bee-sample"
+BEE_PLUGIN_AUTHORS="sschmid"
+BEE_PLUGIN_INFO="A template for bee plugins"
+BEE_PLUGIN_SOURCE_HTTPS="https://github.com/sschmid/bee-sample.git"
+BEE_PLUGIN_SOURCE_SSH="git@github.com:sschmid/bee-sample.git"
+BEE_PLUGIN_TAG="1.0.0"
+BEE_PLUGIN_SHA256="1350c1b5077aa90bb90d9a1fab0b6c68d8861802d5bcddcb138057f67e12c3fa"
+# Uncomment if this plugin has dependencies
+# BEE_PLUGIN_DEPENDENCIES=(android ios version)
